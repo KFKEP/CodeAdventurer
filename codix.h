dd93c06f20f068071a76612974f7545f2d18b5ed
КФКЕП <kfkepbacking@gmail.com>
2024-11-17 22:06:50 +0200

    Update codix.h

--- codix.h
+++ codix.h
@@ -3,25 +3,27 @@
 
 
 //Основні
+void setMap(std::string fileName);
 std::vector<std::vector<char>> readMapFromFile();
 std::vector<std::vector<std::string>> converter(std::vector<char> data);
 
 //Додаткові
-void copyMap();
-void setMap(std::string fileName);
+int checkForBeep();
+int frontCheck();
 
 //Системні
 void placePlayerOnMap(std::vector<std::vector<char>>& map);
 void displayMap(const std::vector<std::vector<char>>& map);
+void copyMap();
 
 void rendering();
 
 void turnRight();
 void turnLeft();
 
-int frontCheck();
+
 
 void step();
 
-int checkForBeep();
+
 void collectBeep();
